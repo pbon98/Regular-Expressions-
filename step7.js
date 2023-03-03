92965e047b96ec497b224cc75c16580f44498c4b
pbon98 <pbaughan15@gmail.com>
2023-03-02 21:02:41 -0500

    Regular.expressions free-code-camp

---
+++ step7.js
@@ -0,0 +1,5 @@
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /run.|sun.|fun.|pun.|nun.|bun./; // Change this line
+let result = unRegex.test(exampleStr);
+
+console.log(exampleStr)
